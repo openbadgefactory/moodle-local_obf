92285ae7caf7a2b7edc9392ec5a2b028a177f2dd
Antti Koskinen <antti.koskinen@obfsolutions.com>
2025-12-01 13:16:56 +0200

    Disable certificate_expiration_reminder cron task

--- classes/task/certificate_expiration_reminder.php
+++ classes/task/certificate_expiration_reminder.php
@@ -39,55 +39,7 @@ class certificate_expiration_reminder extends \core\task\scheduled_task {
     }
 
     public function execute() {
-        global $CFG;
-
-        require_once($CFG->dirroot . '/local/obf/classes/client.php');
-        require_once($CFG->libdir . '/messagelib.php');
-        require_once($CFG->libdir . '/datalib.php');
-
-        $certexpiresin = obf_client::get_instance()->get_certificate_expiration_date();
-        if (!$certexpiresin) {
-            return true;
-        }
-
-        $diff = $certexpiresin - time();
-        $days = floor($diff / (60 * 60 * 24));
-
-        // Notify only if there's certain amount of days left before the certification expires.
-        $notify = in_array($days, array(30, 25, 20, 15, 10, 5, 4, 3, 2, 1));
-
-        if (!$notify) {
-            return true;
-        }
-
-        $severity = $days <= 5 ? 'errors' : 'notices';
-        $admins = get_admins();
-        $textparams = new stdClass();
-        $textparams->days = $days;
-        $textparams->obfurl = obf_client::get_site_url(); // FIXME missing client id.
-        $textparams->configurl = (string) (new \moodle_url('/local/obf/config.php'));
-
-        foreach ($admins as $admin) {
-            $eventdata = new \core\message\message();
-            $eventdata->component = 'moodle';
-            $eventdata->name = $severity;
-            $eventdata->userfrom = $admin;
-            $eventdata->userto = $admin;
-            $eventdata->subject = get_string('expiringcertificatesubject',
-                'local_obf');
-            $eventdata->fullmessage = get_string('expiringcertificate', 'local_obf',
-                $textparams);
-            $eventdata->fullmessageformat = FORMAT_MARKDOWN;
-            $eventdata->fullmessagehtml = get_string('expiringcertificate',
-                'local_obf', $textparams);
-            $eventdata->smallmessage = get_string('expiringcertificatesubject',
-                'local_obf');
-
-            $result = message_send($eventdata);
-        }
-
         return true;
     }
-
 }
 
